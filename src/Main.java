5efd2c0d21c553049322c692316340d991d0730c
etiti <christopherobinna27@gmail.com>
2024-06-24 01:43:42 +0200

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Test infor = new Test("Obinna", 29);
+        System.out.println(infor.getName());
+        infor.setName("Christop");
+        System.out.println(infor.getName());
+
+
+        System.out.println("Hello world!");
+    }
+}
